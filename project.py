2057eaab69a13e8ec2122f087d999950d9b5a858
Jamie Palatnik <jamiechristensen9@gmail.com>
2025-03-18 23:03:58 -0400

    Add function to remove duplicate links on request

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import cowsay
 import sys
 
+
 def main():
     cowsay.cow("Welcome to Bookmark Analyzer!")
 
@@ -13,36 +14,54 @@ def main():
         try:
             if not sys.argv[1].endswith(".txt"):
                 raise ValueError
-            input = sys.argv[1]
-            output = sys.argv[2]
-            sorted_list = abc_sort(input)
+            txtfile = sys.argv[1]
+            newtxtfile = sys.argv[2]
+            # Sort the list alphabetically
+            sorted_list = abc_sort(txtfile)
             print(f"There are {len(sorted_list)} items in your list.")
-            write_to_new_file(sorted_list, output)
-            print(f"Check {output} to review your new TXT file.")
+            # # Ask the user if they want to remove duplicates
+            sorted_list = remove_duplicates_on_request(sorted_list)
+            write_to_new_file(sorted_list, newtxtfile)
+            print(f"Check {newtxtfile} to review your new TXT file.")
         except ValueError:
             sys.exit("Not a TXT file")
         except FileNotFoundError:
             sys.exit(f"Could not read {sys.argv[1]}")
 
-def abc_sort(input):
+
+def abc_sort(txtfile):
     """Sort URLs alphabetically"""
     sorted_list = []
-    with open(input, "r") as file:
+    with open(txtfile, "r") as file:
         for line in sorted(file):
             if line.strip() != "":
                 sorted_list.append(line.strip())
         return sorted_list
 
-def remove_duplicates(input):
-    """Remove duplicate URLs"""
-    ...
+
+def remove_duplicates_on_request(txtfile):
+    """Remove duplicate URLs if requested"""
+    answer = input("Would you like to remove duplicate links? ")
+    if answer.lower() == "y" or answer == "yes":
+        seen = []
+        duplicates = set()
+        for i in txtfile:
+            if i in seen:
+                duplicates.add(i)
+            else:
+                seen.append(i)
+        print(f"{len(txtfile) - len(seen)} duplicate links were removed.")
+        print(f"There are {len(seen)} links in your updated list.")
+        return seen
+    else:
+        return txtfile
 
 
-def write_to_new_file(input, output):
+def write_to_new_file(txtfile, newtxtfile):
     """Write TXT to file given in sys.argv[2]"""
-    with open(output, "w") as file:
-        file.write(f"Updated TXT file\n\n")
-        for line in input:
+    with open(newtxtfile, "w") as file:
+        file.write("Updated TXT file\n\n")
+        for line in txtfile:
             file.write(line + "\n")
 
 
